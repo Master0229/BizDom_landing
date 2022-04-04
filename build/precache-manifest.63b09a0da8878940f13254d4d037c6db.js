6db54a4804632996699877ab03f2870bc2ea1c7e
Master0229 <masterservice2020@gmail.com>
2022-04-04 16:39:04 +0530

    #2 Build Folder

---
+++ build/precache-manifest.63b09a0da8878940f13254d4d037c6db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ccc03838b5298fde93cb4f5ef4776ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cee2a712eafd013c7d0",
+    "url": "/static/css/2.39596b15.chunk.css"
+  },
+  {
+    "revision": "fc59fdcf9154eb8ca445",
+    "url": "/static/css/main.7171e894.chunk.css"
+  },
+  {
+    "revision": "6cee2a712eafd013c7d0",
+    "url": "/static/js/2.32f1a5a8.chunk.js"
+  },
+  {
+    "revision": "fc59fdcf9154eb8ca445",
+    "url": "/static/js/main.7dfc3bb0.chunk.js"
+  },
+  {
+    "revision": "11aeb6b7a1e804b39aa8",
+    "url": "/static/js/runtime-main.83978680.js"
+  }
+]);
